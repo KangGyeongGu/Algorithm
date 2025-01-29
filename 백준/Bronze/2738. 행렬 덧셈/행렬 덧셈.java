e363599a4d44895304e1dfc0f7a648413dd2a78f
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-01-29 10:52:48 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 148 ms, Memory: 15736 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,33 +1,37 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        // 첫 줄 입력
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int[][] nArr = new int[N][M];
+        int[][] matrix = new int[N][M];
 
+        // 첫 번째 행렬 입력
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                nArr[i][j] = sc.nextInt();
+                matrix[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        // 두 번째 행렬 입력 및 합 계산
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                nArr[i][j] += sc.nextInt();
+                matrix[i][j] += Integer.parseInt(st.nextToken());
+                sb.append(matrix[i][j]).append(" ");
             }
+            sb.append("\n");
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(nArr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // 결과 출력
+        System.out.print(sb);
     }
 }
