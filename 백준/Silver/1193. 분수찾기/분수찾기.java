8533babf794f5a5d7e1e6d662bb94d9b1c2e9724
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-02-04 13:51:56 +0900

    [Silver V] Title: 분수찾기, Time: 124 ms, Memory: 15964 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,41 +1,26 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int X = Integer.parseInt(br.readLine());
-		
-		int cnt = 0, idx=0;
-		for (int i = 1; i <= 10000000; i++) {
-			if (X <= cnt) {
-				idx = --i;
-				break;
-			}
-			cnt += i;
-		}
-		
-		X -= cnt-idx;
-		
-		if (isEven(idx)) { 
-			int c = 1; int m = idx;
-			for (int i = 0; i < X-1; i++) {
-				c++; m--;
-			}
-			System.out.println(c + "/" + m);
-		}
-		else { 
-			int c = idx; int m = 1; 
-			for (int i = 0; i < X-1; i++) {
-				c--; m++;
-			}
-			System.out.println(c + "/" + m);
-		}
-	}
-	
-	static boolean isEven(int idx) {
-		return idx%2==0 ? true : false;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+
+        int idx = 0, cnt = 0;
+
+        // O(N) → O(√N) 개선: 대각선 줄(idx) 찾기
+        while (cnt < X) {
+            idx++;
+            cnt += idx;
+        }
+
+        X -= (cnt - idx); // X번째 숫자가 현재 대각선에서 몇 번째인지 보정
+
+        if (idx % 2 == 0) { 
+            // 짝수 번째 줄: 분자가 증가하고 분모가 감소
+            System.out.println(X + "/" + (idx - X + 1));
+        } else { 
+            // 홀수 번째 줄: 분자가 감소하고 분모가 증가
+            System.out.println((idx - X + 1) + "/" + X);
+        }
+    }
 }
-		
