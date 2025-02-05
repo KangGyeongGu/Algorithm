c8a8a9b92082b6c83862d4b6f04f5f9882efcab5
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-02-05 17:29:49 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1420 ms, Memory: 31984 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
@@ -22,13 +23,12 @@ public class Main {
 		for (int i = 0; i < K; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < 4; j++) {
-				scope[i][j] = Integer.parseInt(st.nextToken()); 
+				scope[i][j] = Integer.parseInt(st.nextToken());
 			}
+			sb.append(getSum(NM, scope[i])).append('\n');
 		}
 		
-		for (int i = 0; i < K; i++) {
-			System.out.println(getSum(NM, scope[i]));
-		}
+		System.out.println(sb);
 		
 		br.close();
 	}
