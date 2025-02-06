ce5304d338b2638313801b460f425d7b7d63c701
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-02-06 16:15:46 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -4,33 +4,28 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		StringBuilder sb = new StringBuilder();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine());
 		int[][] arr = new int[N][2];
-		
-		for (int i = 0; i < N; i++) {
+
+		for (int r = 0; r < N; r++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < 2; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				
+			for (int c = 0; c < 2; c++) {
+				arr[r][c] = Integer.parseInt(st.nextToken()); 
 			}
 		}
 		
 		for (int i = 0; i < arr.length; i++) {
 			int rank = 1;
 			for (int j = 0; j < arr.length; j++) {
-				if (arr[i][0] < arr[j][0]) {
-					if (arr[i][1] < arr[j][1]) {
-						rank++;
-					}
-				}
-				else continue;
+				if (i == j) continue;
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) rank++;
 			}
 			sb.append(rank).append(" ");
 		}
-		
 		System.out.println(sb);
+		br.close();
 	}
 }
