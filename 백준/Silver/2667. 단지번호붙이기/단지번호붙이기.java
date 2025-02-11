4493cf72b2d27a0fefb188cf4ad3b777e0f04f19
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-02-11 15:54:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,8 +5,15 @@ public class Main {
 	
 	static int[][] arr;
 	static boolean[][] iV;
-	static Integer[][] stack;
+	
+	static Stack<int[]> stack;
+	
+	static int aptCount;
 	static ArrayList<Integer> eachApt = new ArrayList<>();
+	
+	
+	
+	
 	static StringBuilder sb = new StringBuilder();
 	static int[][] deltas = {
 			{0, -1},
@@ -27,57 +34,53 @@ public class Main {
 			String in = br.readLine();
 			for (int c = 0; c < N; c++) {
 				int curC = in.charAt(c) - '0';
-				if (curC == 1) {
-					arr[r][c] = 1; 
-				}
+				if (curC == 1) arr[r][c] = 1; 
 			}
 		}
 		
-		int totalArea = 0;
+		// DFS by Stack
 		for (int r = 0; r < N; r++) {
 			for (int c = 0; c < N; c++) {
 				if (arr[r][c] == 1 && !iV[r][c]) {
-					int aptCnt = 1;
-					int idx = 0;
-					stack = new Integer[N*N][2];
-					
-					// check (r,c) true to iV
-					iV[r][c] = true;
-					
-					// put (r,c) in stack
-					stack[idx][0] = r; stack[idx][1] = c; idx++;
-					
-					// dfs loop
-					while (idx > 0) {
-						// stack pop
-						idx--;
-						int curR = stack[idx][0];
-						int curC = stack[idx][1];
-						
-						// find nearest index
-						for (int i = 0; i < 4; i++) {
-							int nR = curR + deltas[i][0];
-							int nC = curC + deltas[i][1];
-							
-							if ((0<=nR && nR < N && 0 <= nC && nC < N)  && (arr[nR][nC] == 1 && !iV[nR][nC])) {
-								iV[nR][nC] = true;
-								stack[idx][0] = nR; stack[idx][1] = nC; idx++;
-								aptCnt++;
-							}
-						}
-					} // area search quit
-					eachApt.add(aptCnt);
-					totalArea++;
+					dfs(r, c);
 				}
 			}
 		}
+		
 		Collections.sort(eachApt);
-		sb.append(totalArea).append('\n');
-		for (int i = 0; i < eachApt.size(); i++) {
-			sb.append(eachApt.get(i)).append('\n');
-		}
+		
+		sb.append(eachApt.size()).append('\n');
+		for (int i = 0; i < eachApt.size(); i++) sb.append(eachApt.get(i)).append('\n');
 		
 		System.out.println(sb);
 		br.close();
+	}
+	
+	private static void dfs(int startX, int startY) {
+		//init Var
+		aptCount = 1;
+		stack = new Stack<>();
+		
+		// init start node
+		stack.push(new int[] {startX, startY});
+		iV[startX][startY] = true;
+		
+		while (!stack.isEmpty()) {
+			int[] curP = stack.pop();
+			int x = curP[0]; int y = curP[1];
+			
+			for (int i = 0; i < 4; i++) {
+				int nR = x + deltas[i][0]; int nC = y + deltas[i][1];
+				
+				if (0<=nR && nR < iV.length && 0 <= nC && nC < iV.length && arr[nR][nC] == 1 && !iV[nR][nC]) {
+					if (!iV[nR][nC]) {
+						stack.push(new int[]{nR,nC});
+						iV[nR][nC] = true; 
+						aptCount++;
+					}
+				}
+			}
+		}
+		eachApt.add(aptCount);
 	}
 }
