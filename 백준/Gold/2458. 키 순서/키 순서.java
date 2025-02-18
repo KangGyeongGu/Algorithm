65419bb177549e16414b1ddd3a613b074af05123
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-02-18 22:58:15 +0900

    [Gold IV] Title: 키 순서, Time: 644 ms, Memory: 42728 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -2,60 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static List<Integer>[] adj, revAdj;
-    static boolean[] visited;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    
+    static int N, M, result;
+    static boolean[] iV;
+    static List<ArrayList<Integer>> adj = new ArrayList<>(), radj = new ArrayList<>();
     
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
+        init();
+        run();
+        System.out.println(result);
+    }
+    
+    private static void run() {
+    	for (int i = 1; i <= N; i++) {
+			iV = new boolean[N+1];
+			int taller = dfs(i, adj);
+			
+			iV = new boolean[N+1];
+			int smaller = dfs(i, radj);
+			
+			if (taller+smaller == N-1) result++;
+		}
+    }
+    
+    private static int dfs(int node, List<ArrayList<Integer>> graph) {
+    	iV[node] = true;
+    	int count = 0;
+    	
+    	for(int vertex : graph.get(node)) {
+    		if (!iV[vertex]) {
+    			count += 1 + dfs(vertex, graph);
+    		}
+    	}
+    	
+    	return count;
+    }
+    
+    private static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         
-        adj = new ArrayList[N + 1];
-        revAdj = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            adj[i] = new ArrayList<>();
-            revAdj[i] = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            adj.add(new ArrayList<>());
+            radj.add(new ArrayList<>());
         }
-
-        // 입력 받기 (그래프 구성)
+        
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            adj[a].add(b);       // 정방향 그래프
-            revAdj[b].add(a);    // 역방향 그래프 (역순 탐색용)
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adj.get(u).add(v);
+            radj.get(v).add(u);
         }
-
-        int result = 0;
-        for (int i = 1; i <= N; i++) {
-            visited = new boolean[N + 1];
-            int smallerCount = dfs(i, adj); // 자신보다 작은 학생 수 탐색
-
-            visited = new boolean[N + 1];
-            int tallerCount = dfs(i, revAdj); // 자신보다 큰 학생 수 탐색
-
-            if (smallerCount + tallerCount == N - 1) {
-                result++;
-            }
-        }
-
-        System.out.println(result);
-    }
-
-    // DFS 탐색 (주어진 방향 그래프 탐색)
-    static int dfs(int node, List<Integer>[] graph) {
-        visited[node] = true;
-        int count = 0;
-        
-        for (int next : graph[node]) {
-            if (!visited[next]) {
-                count += 1 + dfs(next, graph);
-            }
-        }
-        
-        return count;
     }
 }
