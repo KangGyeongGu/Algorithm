63f92950687a9599583dc9538e37b6e6b4d39533
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-02-26 20:18:01 +0900

    [Gold V] Title: 회의실 배정, Time: 500 ms, Memory: 51936 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -7,7 +7,9 @@ public class Main {
     static StringTokenizer st;
 
     static int N, count;
-    static List<int[]> list = new ArrayList<>();
+    static PriorityQueue<int[]> pq = new PriorityQueue<>(
+        (o1, o2) -> o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0]
+    );
 
     public static void main(String[] args) throws IOException {
         init();
@@ -16,10 +18,11 @@ public class Main {
     }
 
     private static void greedy() {
-        int[] cur = list.get(0); count = 1;
-        
-        for (int i = 1; i < list.size(); i++) {
-            int[] next = list.get(i);
+        int[] cur = pq.poll();
+        count = 1;
+
+        while (!pq.isEmpty()) {
+            int[] next = pq.poll();
             if (cur[1] <= next[0]) {
                 count++;
                 cur = next;
@@ -31,8 +34,7 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         while (N-- > 0) {
             String[] in = br.readLine().split(" ");
-            list.add(new int[]{Integer.parseInt(in[0]), Integer.parseInt(in[1])});
+            pq.add(new int[] {Integer.parseInt(in[0]), Integer.parseInt(in[1])});
         }
-        Collections.sort(list, (o1, o2) -> o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0]);
     }
 }
