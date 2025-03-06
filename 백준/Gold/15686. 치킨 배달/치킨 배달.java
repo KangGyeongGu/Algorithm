1bde1700e0a01785ff676ee9dd4c74fab979d309
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-03-06 22:01:17 +0900

    [Gold V] Title: 치킨 배달, Time: 152 ms, Memory: 15732 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -11,80 +11,78 @@ public class Main {
         }
     }
     
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    
     static int N, M;
-    static int[][] map;
-    static boolean[] isChikens;
     static List<Node> chickens = new ArrayList<>();
     static List<Node> houses = new ArrayList<>();
-    static List<Node> chickenCombi = new ArrayList<>();
-    
-    static int minChickenDistance = Integer.MAX_VALUE; 
+    static boolean[] selected;
+    static int[][] distances;
+    static int minChickenDistance = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-        init(); 
-        run(0, 0); 
+        init();  
+        selectChickens(0, 0);
         System.out.println(minChickenDistance);
     }
 
-    private static void run(int start, int depth) {
+    private static void selectChickens(int start, int depth) {
         if (depth == M) { 
             minChickenDistance = Math.min(minChickenDistance, calcCityChickenDistance());
             return;
         }
         
         for (int i = start; i < chickens.size(); i++) {
-            if (isChikens[i]) continue;
+            if (!selected[i]) {
+                selected[i] = true;
+                selectChickens(i + 1, depth + 1);
+                selected[i] = false;
+            }
+        }
+    }
 
-            isChikens[i] = true;
-            chickenCombi.add(chickens.get(i));
-            run(i + 1, depth + 1);
-            chickenCombi.remove(chickenCombi.size() - 1); 
-            isChikens[i] = false;
+    private static void precomputeDistances() {
+        int hSize = houses.size();
+        int cSize = chickens.size();
+        distances = new int[hSize][cSize];
+
+        for (int i = 0; i < hSize; i++) {
+            for (int j = 0; j < cSize; j++) {
+                distances[i][j] = Math.abs(houses.get(i).x - chickens.get(j).x) +
+                                  Math.abs(houses.get(i).y - chickens.get(j).y);
+            }
         }
     }
 
     private static int calcCityChickenDistance() {
         int totalDistance = 0;
 
-        for (Node house : houses) {
-            int minDistance = Integer.MAX_VALUE;
-
-            for (Node chicken : chickenCombi) {
-                int distance = calcDistance(house.x, house.y, chicken.x, chicken.y);
-                minDistance = Math.min(minDistance, distance);
+        for (int i = 0; i < houses.size(); i++) {
+            int minDist = Integer.MAX_VALUE;
+            for (int j = 0; j < chickens.size(); j++) {
+                if (selected[j]) {
+                    minDist = Math.min(minDist, distances[i][j]);
+                }
             }
-
-            totalDistance += minDistance;
+            totalDistance += minDist;
         }
-
         return totalDistance;
     }
 
-    private static int calcDistance(int x1, int y1, int x2, int y2) {
-        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
-    }
-
     private static void init() throws IOException {
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
-        map = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                int in = Integer.parseInt(st.nextToken());
-                map[i][j] = in;
-
-                if (in == 1) houses.add(new Node(i, j)); 
-                if (in == 2) chickens.add(new Node(i, j)); 
+                int value = Integer.parseInt(st.nextToken());
+                if (value == 1) houses.add(new Node(i, j));
+                else if (value == 2) chickens.add(new Node(i, j));
             }
         }
 
-        isChikens = new boolean[chickens.size()];
+        selected = new boolean[chickens.size()];
+        precomputeDistances();
     }
 }
