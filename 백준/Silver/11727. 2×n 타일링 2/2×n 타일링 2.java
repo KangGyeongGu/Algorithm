f59247c74f14169e60fff22f7de3469f5cff9d1b
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-03-14 09:36:45 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -12,15 +12,28 @@ public class Main {
 		
 		dp = new int[N+1];
 		
-		System.out.println(dp(N));
+//		System.out.println(dp(N));
+		System.out.println(bottomUp(N));
 		
 	}
 	
-	private static int dp(int n) {
+	private static int topDown(int n) {
 		if (n == 1) return 1;
 		if (n == 2) return 3;
 		if (dp[n]!=0) return dp[n];
 		
-		return dp[n] = (dp(n-1) + 2 * dp(n-2)) % 10007;
+		return dp[n] = (topDown(n-1) + 2 * topDown(n-2)) % 10007;
+	}
+	
+	private static int bottomUp(int n) {
+		dp[1] = 1;
+		
+		if (N >= 2) dp[2] = 3;
+		
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (dp[i-1] + 2 * dp[i-2]) % 10007;
+		}
+		
+		return dp[N];
 	}
 }
