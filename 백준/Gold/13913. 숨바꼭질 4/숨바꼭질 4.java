c2504ba94cbcee0df27a097514851ff543acfa18
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-03-14 16:42:16 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 224 ms, Memory: 25184 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -3,13 +3,14 @@ import java.util.*;
 
 public class Main {
 	
+	static StringBuilder sb = new StringBuilder();
+	
 	static final int UPPERBOUND = 100001;
 
-	static int N, K;
-	static List<Integer> route = new ArrayList<>();
-	
+	static int N, K, count;
+	static int[] route;
 	static int[] parent = new int[UPPERBOUND];
-	static boolean[] iv = new boolean[UPPERBOUND];
+	static int[] iv = new int[UPPERBOUND];
 	static Queue<Integer> Q = new ArrayDeque<>();
 	
 	public static void main(String[] args) throws IOException {
@@ -21,19 +22,27 @@ public class Main {
 
 		bfs();
 		
-		for (int i = K; i != -1; i = parent[i]) route.add(i);
-		Collections.reverse(route);
+		count = iv[K];
+		sb.append(count).append('\n');
 		
-		System.out.println(route.size()-1);
-		for (int e : route) {
-			System.out.print(e + " ");
+		route = new int[count+1];
+		int pNode = K;
+		
+		while (pNode != N) {
+			route[count--] = pNode;
+			pNode = parent[pNode];
 		}
+		route[count] = N;
+		
+		for (int i = 0; i < route.length; i++) {
+			sb.append(route[i]).append(' ');
+		}
+		
+		System.out.println(sb);
 	}
 	
 	private static void bfs() {
 		Q.offer(N);
-		iv[N] = true;
-		parent[N] = -1;
 
 		while (!Q.isEmpty()) {
 			int cur = Q.poll();
@@ -41,9 +50,9 @@ public class Main {
 			if (cur == K) return;
 			
 			for (int child : new int[] {cur-1, cur+1, cur*2}) {
-				if (0 <= child && child < UPPERBOUND && !iv[child]) {
+				if (0 <= child && child < UPPERBOUND && iv[child] == 0) {
 					Q.offer(child);
-					iv[child] = true;
+					iv[child] = iv[cur] + 1;
 					parent[child] = cur; 
 				}
 			}
