bd5f86a0ae01b65696298341c4b8147d85c0e027
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-03-27 10:21:39 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 428 ms, Memory: 47240 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,84 +3,77 @@ import java.io.*;
 
 public class Main {
 
-	static class Edge implements Comparable<Edge> {
-		int u;
-		int v;
-		int w;
-		public Edge(int u, int v, int w) {
-			super();
-			this.u = u;
-			this.v = v;
-			this.w = w;
-		}
-		
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.w, o.w);
-		}
-	}
-	
-	static int V, E;
-	static int[] sets;
-	static List<Edge> list = new ArrayList<>();
-	
-	
-	public static void main(String[] args) throws IOException {
-		init();
-		kruskal();
-	}
-	
-	static void kruskal() {
-		int CNT = 0, ANS = 0;
-		
-		for (int i = 0; i < E; i++) {
-			Edge curE = list.get(i);
-			
-			if (!union(curE.u, curE.v)) continue;
-			
-			ANS += curE.w;
-			CNT++;
-			
-			if (CNT == V-1) break;
-		}
-		
-		System.out.println(ANS);
-	}
-	
-	static int find(int x) {
-		if (sets[x] < 0) return x;
-		return sets[x] = find(sets[x]);
-	}
-	
-	static boolean union(int a, int b) {
-		a = find(a);
-	    b = find(b);
+    static class Edge implements Comparable<Edge> {
+        int u, v, w;
 
-	    if (a == b) return false;
-	    if (sets[a] == sets[b]) sets[a]--;
-	    if (sets[a] < sets[b]) sets[b] = a;
-	    else sets[a] = b;
-	    return true;
-	}
-	
-	static void init() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        public Edge(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+
+    static int V, E;
+    static int[] sets;
+    static PriorityQueue<Edge> pq;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        System.out.println(kruskal());
+    }
+
+    static int kruskal() {
+        int cnt = 0, ans = 0;
+
+        while (!pq.isEmpty() && cnt < V - 1) {
+            Edge curE = pq.poll();
+
+            if (!union(curE.u, curE.v)) continue;
+
+            ans += curE.w;
+            cnt++;
+        }
+
+        return ans;
+    }
+
+    static int find(int x) {
+        if (sets[x] < 0) return x;
+        return sets[x] = find(sets[x]);
+    }
+
+    static boolean union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a == b) return false;
+        if (sets[a] == sets[b]) sets[a]--;
+        if (sets[a] < sets[b]) sets[b] = a;
+        else sets[a] = b;
+        return true;
+    }
+
+    static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
-        
-        for (int i = 0; i < E; i++) {
+
+        sets = new int[V + 1];
+        Arrays.fill(sets, -1);
+        pq = new PriorityQueue<>();
+
+        while (E-- > 0) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-
-            list.add(new Edge(u, v, w));
+            pq.add(new Edge(u, v, w));
         }
-        
-        Collections.sort(list);
-        
-        sets = new int[V+1];
-        Arrays.fill(sets, -1);
-	}
+    }
 }
