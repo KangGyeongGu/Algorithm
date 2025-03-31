218a3e0290cfca557e405f1adba6b8e76b516f86
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-03-31 15:12:36 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,597 ms, Memory: 171,508 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,85 +2,77 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-
-	static class Edge implements Comparable<Edge> {
-		int u, v, w;
-
-		public Edge(int u, int v, int w) {
-			super(); this.u = u; this.v = v; this.w = w;
-		}
-		
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.w, o.w);
-		}
-	}
-	
-	static int T, V, E;
-	static int[] sets;
-	static PriorityQueue<Edge> pq = new PriorityQueue<>();
-	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException {
-		
-		T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			init(tc);
-			kruskal(tc);
-		}
-		System.out.println(sb);
-	}
-	
-	private static void kruskal(int tc) {
-		long ans = 0;
+    
+    static class Edge implements Comparable<Edge> {
+        int v, w;
+        public Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+    
+    static int T, V, E;
+    static List<List<Edge>> graph;
+    static boolean[] visited;
+    static PriorityQueue<Edge> pq;
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            init();
+            prim(tc);
+        }
+        System.out.println(sb);
+    }
+    
+    private static void prim(int tc) {
+        long ans = 0;
         int cnt = 0;
-		
-		while (!pq.isEmpty() && cnt < V-1) {
-			Edge curE = pq.poll();
-			
-			if(!union(curE.u, curE.v)) continue;
-			
-			ans += curE.w;
-			cnt++;
-		}
-		
-		sb.append("#" + tc + " " + ans + '\n');
-	}
-	
-	private static int find(int x) {
-		if (sets[x] < 0) return x;
-		return sets[x] = find(sets[x]);
-	}
-	
-	private static boolean union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		
-		if (a==b) return false;
-		if (sets[a]==sets[b]) sets[a]--;
-		if(sets[a]<sets[b]) sets[b] = a;
-		else sets[a] = b;
-		return true;
-	}
-	
-	private static void init(int tc) throws IOException {
-		st = new StringTokenizer(br.readLine());
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		pq.clear();
-		for (int e = 0; e < E; e++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			pq.offer(new Edge(u, v, w));
-		}
-		
-		sets = new int[V+1];
-		Arrays.fill(sets, -1);
-	}
+        
+        pq.offer(new Edge(1, 0)); 
+        while (!pq.isEmpty() && cnt < V) {
+            Edge cur = pq.poll();
+            
+            if (visited[cur.v]) continue;
+            
+            visited[cur.v] = true;
+            ans += cur.w;
+            cnt++;
+            
+            for (Edge next : graph.get(cur.v)) {
+                if (!visited[next.v]) pq.offer(next);
+            }
+        }
+        
+        sb.append("#").append(tc).append(" ").append(ans).append("\n");
+    }
+    
+    private static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        
+        graph = new ArrayList<>();
+        for (int i = 0; i <= V; i++) graph.add(new ArrayList<>());
+        
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph.get(u).add(new Edge(v, w));
+            graph.get(v).add(new Edge(u, w));
+        }
+        
+        visited = new boolean[V + 1];
+        pq = new PriorityQueue<>();
+    }
 }
