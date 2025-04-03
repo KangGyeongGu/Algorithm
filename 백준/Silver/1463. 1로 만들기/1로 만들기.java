94b4763b9b3e8e2a5d8f8dd97219be142aeadc05
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-04 00:21:12 +0900

    [Silver III] Title: 1로 만들기, Time: 192 ms, Memory: 21688 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,38 +1,43 @@
 import java.io.*;
+import java.util.*;
+
+/*
+ * 1. DP : bottom-up + memo
+ * 
+ * 2. 부분 문제 식별
+ * 	- i : 주어진 정수 값
+ *  - D[i] : i 값을 1로 만드는 연산의 최소 횟수
+ * D[0] = 연산 없음 = 0
+ * D[1] = 연산 없음 = 0
+ * D[2] = x-1 || x/2 = 1
+ * D[3] = x/3 = 1
+ * D[4] = x/2/2 || x/3-1 = 2
+ * D[5] = x/2/2-1 || x/3-1-1 = 3
+ * 
+ * 3. 점화식
+ * f(n) = min(f(n-1)+1, f(n/3)+1, f(n/2)+1)
+ * 
+ * 4. 초기 조건
+ * dp[0] = 0, dp[1] = 0
+ * 
+ * */
+
 
 public class Main {
-	
-	static int N;
-	static Integer[] dp;
-	
+
 	public static void main(String[] args) throws IOException {
-		init();
-		System.out.println(dp(N));
-	}
-	
-	static int dp(int N) {
-		if (dp[N] == null) {
-			if (N%6==0) {
-				dp[N] = Math.min(dp(N-1), Math.min(dp(N/2), dp(N/3))) + 1;
-			}
-			else if (N%3==0) {
-				dp[N] = Math.min(dp(N/3), dp(N-1)) + 1; 
-			}
-			else if (N%2==0) {
-				dp[N] = Math.min(dp(N/2), dp(N-1)) + 1;
-			}
-			else {
-				dp[N] = dp(N-1) + 1;
-			}
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] dp = new int[N+1];
+		
+		dp[0] = dp[1] = 0;
+		
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i-1] + 1;
+			if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2]+1);
+			if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3]+1);
 		}
-		return dp[N];
-	}
-	
-	static void init() throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		dp = new Integer[N+1]; dp[0] = dp[1] = 0;
-		br.close();
+		
+		System.out.println(dp[N]);
 	}
 }
-
