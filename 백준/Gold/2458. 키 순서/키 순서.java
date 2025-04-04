6c8cd6ab887be1d3155f10c208e3b76f5e7b3ad8
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-04 15:23:32 +0900

    [Gold IV] Title: 키 순서, Time: 404 ms, Memory: 21900 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,62 +1,67 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    
-    static int N, M, result;
-    static boolean[] iV;
-    static List<ArrayList<Integer>> adj = new ArrayList<>(), radj = new ArrayList<>();
-    
-    public static void main(String[] args) throws IOException {
-        init();
-        run();
-        System.out.println(result);
-    }
-    
-    private static void run() {
-    	for (int i = 1; i <= N; i++) {
-			iV = new boolean[N+1];
+	
+	static int N, M, ANS;
+	static boolean[] iv;
+	static List<List<Integer>> adj, radj;
+	
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while (( c = System.in.read()) >= 48) n = (n << 3) + (n << 1) + (c & 15);
+		if (c==13) System.in.read();
+		return n;
+	}
+	
+	private static void init() throws Exception {
+		N = read();
+		M = read();
+		
+		adj = new ArrayList<>();
+		radj = new ArrayList<>();
+		
+		for (int i = 0; i <= N; i++) {
+			adj.add(new ArrayList<>());
+			radj.add(new ArrayList<>());
+		}
+		
+		for (int i = 0; i < M; i++) {
+			int a = read();
+			int b = read();
+			adj.get(a).add(b);
+			radj.get(b).add(a);
+		}
+	}
+	
+	private static int dfs(int node, List<List<Integer>> graph) {
+		iv[node] = true;
+		int count = 0;
+		
+		for (int next : graph.get(node)) {
+			if (iv[next]) continue;
+			
+			count += 1 + dfs(next, graph);
+		}
+		
+		return count;
+	}
+	
+	private static void run() {
+		for (int i = 1; i <= N; i++) {
+			iv = new boolean[N+1];
 			int taller = dfs(i, adj);
 			
-			iV = new boolean[N+1];
+			iv = new boolean[N+1];
 			int smaller = dfs(i, radj);
 			
-			if (taller+smaller == N-1) result++;
+			if (taller + smaller == N-1) ANS++;
 		}
-    }
-    
-    private static int dfs(int node, List<ArrayList<Integer>> graph) {
-    	iV[node] = true;
-    	int count = 0;
-    	
-    	for(int vertex : graph.get(node)) {
-    		if (!iV[vertex]) {
-    			count += 1 + dfs(vertex, graph);
-    		}
-    	}
-    	
-    	return count;
-    }
-    
-    private static void init() throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        
-        for (int i = 0; i <= N; i++) {
-            adj.add(new ArrayList<>());
-            radj.add(new ArrayList<>());
-        }
-        
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            adj.get(u).add(v);
-            radj.get(v).add(u);
-        }
-    }
+	}
+	
+	public static void main(String[] args) throws Exception {
+		init();
+		run();
+		System.out.println(ANS);
+	}
 }
