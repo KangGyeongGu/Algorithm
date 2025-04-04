4edda031ae429f6cf7a6f8380ed596db0ce70c66
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-04 16:29:17 +0900

    [Gold III] Title: ACM Craft, Time: 392 ms, Memory: 45320 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -3,73 +3,69 @@ import java.util.*;
 
 public class Main {
 	
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while (( c = System.in.read()) >= 48) n = (n << 3) + (n << 1) + (c & 15);
+		if (c==13) System.in.read(); 
+		return n;
+	}
+	
 	static int T, N, K, W;
 	static int[] buildTime, inDegrees, result;
 	static List<List<Integer>> adj;
-	static Queue<Integer> Q;
 	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	
-	public static void main(String[] args) throws IOException {
-		T = Integer.parseInt(br.readLine());
-		while(T-->0) {
-			init();
-			sb.append(bfs()).append('\n');
+	private static void init() throws Exception {
+		N = read();
+		K = read();
+		
+		buildTime = new int[N + 1];
+		result = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			buildTime[i] = read();
+			result[i] = buildTime[i];
 		}
-		System.out.println(sb);
+		
+		adj = new ArrayList<>(N + 1);
+		for (int i = 0; i <= N; i++) {
+			adj.add(new ArrayList<>());
+		}
+		
+		inDegrees = new int[N + 1];
+		for (int i = 0; i < K; i++) {
+			int u = read();
+			int v = read();
+			adj.get(u).add(v);
+			inDegrees[v]++;
+		}
+		
+		W = read();
 	}
 	
-	private static int bfs() {
+	private static int topologicalSort() {
+		Queue<Integer> Q = new ArrayDeque<>();
+		for (int i = 1; i <= N; i++) {
+			if (inDegrees[i] == 0) Q.offer(i);
+		}
 		
 		while (!Q.isEmpty()) {
 			int cur = Q.poll();
 			
 			for (int next : adj.get(cur)) {
 				result[next] = Math.max(result[next], result[cur] + buildTime[next]);
-				if (--inDegrees[next] == 0) {
-					Q.offer(next);
-				}
+				if (--inDegrees[next] == 0) Q.offer(next);
 			}
 		}
 		
 		return result[W];
 	}
 	
-	private static void init() throws IOException {
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		buildTime = new int[N+1];
-		result = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			buildTime[i] = Integer.parseInt(st.nextToken());
-			result[i] = buildTime[i];
+	public static void main(String[] args) throws Exception {
+		T = read();
+		while (T-- > 0) {
+			init();
+			bw.write(topologicalSort() + "\n");
 		}
-		
-		adj = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
-			adj.add(new ArrayList<>());
-		}
-		
-		inDegrees = new int[N+1];
-		for (int i = 0; i < K; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			adj.get(u).add(v);
-			inDegrees[v]++;
-		}
-		
-		W = Integer.parseInt(br.readLine());
-		
-		Q = new ArrayDeque<>();
-		for (int i = 1; i <= N; i++) {
-			if (inDegrees[i]==0) Q.offer(i);
-		}
-
+		bw.flush(); bw.close();
 	}
 }
