5ea5103d24e6eda28be7f8ac2289813c01292357
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-04 14:06:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 차량 정비소, Time: 125 ms, Memory: 31,424 KB -BaekjoonHub

--- SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.java
+++ SWEA/Unrated/2477. ［모의 SW 역량테스트］ 차량 정비소/［모의 SW 역량테스트］ 차량 정비소.java
@@ -6,10 +6,8 @@ public class Solution {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
-    static class Customer implements Comparable<Customer> {
-        int customerId, recepId, repairId, arrivedTime;
-        int remainingTime; // 현재 창구에서 남은 처리 시간
-        boolean inRecep, inRepair;
+    static class Customer {
+        int customerId, recepId, repairId, arrivedTime, remainingTime;
 
         public Customer(int customerId, int arrivedTime) {
             this.customerId = customerId;
@@ -17,21 +15,13 @@ public class Solution {
             this.recepId = -1;
             this.repairId = -1;
             this.remainingTime = 0;
-            this.inRecep = false;
-            this.inRepair = false;
-        }
-
-        @Override
-        public int compareTo(Customer o) {
-            return Integer.compare(this.arrivedTime, o.arrivedTime);
         }
     }
 
-    static int T, N, M, K, A, B, recepTime[], repairTime[], visitTime[];
-    static boolean[] recepBusy, repairBusy;
-    static Queue<Customer> waitingRecep, waitingRepair;
+    static int T, N, M, K, A, B;
+    static int[] recepTime, repairTime;
+    static ArrayDeque<Customer> waitingRecep, waitingRepair, customerQueue;
     static Customer[] recepDesk, repairDesk;
-    static List<Customer> customers;
     static int answer;
 
     private static void init() throws IOException {
@@ -44,7 +34,6 @@ public class Solution {
 
         recepTime = new int[N + 1];
         repairTime = new int[M + 1];
-        visitTime = new int[K + 1];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) recepTime[i] = Integer.parseInt(st.nextToken());
@@ -52,36 +41,29 @@ public class Solution {
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= M; i++) repairTime[i] = Integer.parseInt(st.nextToken());
 
+        customerQueue = new ArrayDeque<>();
         st = new StringTokenizer(br.readLine());
-        customers = new ArrayList<>();
         for (int i = 1; i <= K; i++) {
-            visitTime[i] = Integer.parseInt(st.nextToken());
-            customers.add(new Customer(i, visitTime[i]));
+            customerQueue.offer(new Customer(i, Integer.parseInt(st.nextToken())));
         }
 
-        recepBusy = new boolean[N + 1];
-        repairBusy = new boolean[M + 1];
-
-        waitingRecep = new LinkedList<>();
-        waitingRepair = new LinkedList<>();
-
+        waitingRecep = new ArrayDeque<>();
+        waitingRepair = new ArrayDeque<>();
         recepDesk = new Customer[N + 1];
         repairDesk = new Customer[M + 1];
 
         answer = 0;
     }
 
-    private static void bfs() {
+    private static void simulate() {
         int time = 0;
-        int servedCustomers = 0; // 처리된 고객 수
+        int servedCustomers = 0;
 
         while (servedCustomers < K) {
-        	
-            // 1. 도착한 고객을 접수 대기열에 추가
-            for (Customer customer : customers) {
-                if (customer.arrivedTime == time) {
-                    waitingRecep.offer(customer);
-                }
+
+            // 1. 도착한 고객을 접수 대기 큐에 추가
+            while (!customerQueue.isEmpty() && customerQueue.peek().arrivedTime == time) {
+                waitingRecep.offer(customerQueue.poll());
             }
 
             // 2. 접수 창구 배정
@@ -90,18 +72,17 @@ public class Solution {
                     Customer customer = waitingRecep.poll();
                     customer.recepId = i;
                     customer.remainingTime = recepTime[i];
-                    customer.inRecep = true;
                     recepDesk[i] = customer;
                 }
             }
 
-            // 3. 접수 진행 중인 고객 처리
+            // 3. 접수 창구 진행
             for (int i = 1; i <= N; i++) {
                 if (recepDesk[i] != null) {
                     recepDesk[i].remainingTime--;
                     if (recepDesk[i].remainingTime == 0) {
-                        waitingRepair.offer(recepDesk[i]);
-                        recepDesk[i] = null; // 창구 비우기
+                        waitingRepair.offer(recepDesk[i]); // 정비 대기열로 이동
+                        recepDesk[i] = null;
                     }
                 }
             }
@@ -112,12 +93,11 @@ public class Solution {
                     Customer customer = waitingRepair.poll();
                     customer.repairId = i;
                     customer.remainingTime = repairTime[i];
-                    customer.inRepair = true;
                     repairDesk[i] = customer;
                 }
             }
 
-            // 5. 정비 진행 중인 고객 처리
+            // 5. 정비 창구 진행
             for (int i = 1; i <= M; i++) {
                 if (repairDesk[i] != null) {
                     repairDesk[i].remainingTime--;
@@ -126,12 +106,12 @@ public class Solution {
                             answer += repairDesk[i].customerId;
                         }
                         servedCustomers++;
-                        repairDesk[i] = null; // 창구 비우기
+                        repairDesk[i] = null;
                     }
                 }
             }
 
-            time++; 
+            time++;
         }
     }
 
@@ -139,7 +119,7 @@ public class Solution {
         T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
             init();
-            bfs();
+            simulate();
             sb.append("#").append(tc).append(" ").append(answer == 0 ? -1 : answer).append('\n');
         }
         System.out.println(sb);
