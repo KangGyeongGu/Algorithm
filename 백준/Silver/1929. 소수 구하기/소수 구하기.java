0e7d847df50009737318f995ff151a128658c332
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-05 19:05:27 +0900

    [Silver III] Title: 소수 구하기, Time: 212 ms, Memory: 22964 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,37 +1,37 @@
 import java.io.*;
 
 public class Main {
-	
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	private static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while ((c = System.in.read()) >= 48) n = (n << 3) + (n << 1) + (c & 15);
-		if (c == 13) System.in.read();
-		return n;
-	}
 
-	public static void main(String[] args) throws Exception {
-		
-		int M = read(), N = read();
-		
-		for (int i = M; i <= N; i++) {
-			
-			if (i < 2) continue;
-			
-			boolean flag = true;
-			
-			for (int j = 2; j*j <= i; j++) {
-				if (i % j == 0) { 
-					flag = false; 
-					break; 
-				}
-			}
-			
-			if (flag) bw.write(i + "\n");
-			
-		}
-		
-		bw.flush(); bw.close();
-	}
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48) n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13) System.in.read();
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int M = read(), N = read();
+        boolean[] isNotPrime = new boolean[N + 1];
+
+        isNotPrime[0] = isNotPrime[1] = true;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (!isNotPrime[i]) {
+                bw.write(i + "\n");
+            }
+        }
+
+        bw.flush(); bw.close();
+    }
 }
