6a2c5dc5dfc8ce80d6b6306d818ebd5db7519960
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-08 10:10:04 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 112 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -2,38 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	private static int read() throws Exception {
-		int c, n = System.in.read() & 15;
-		while (( c = System.in.read()) >= 48) n = (n << 3) + (n << 1) + (c & 15);
-		if (c==13) System.in.read(); 
-		return n;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		int N = read();
-		int[] arr = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = read();
-		}
-		
-		int ANS = 0;
-		
-		int[] dp = new int[N];
-		for (int i = 0; i < N; i++) {
-			dp[i] = 1;
-			
-			for (int j = 0; j < i; j++) {
-				if (arr[j] < arr[i]) {
-					dp[i] = Math.max(dp[i], dp[j]+1);
-				}
-			}
-			
-			ANS = Math.max(ANS, dp[i]);
-		}
-		
-		bw.write(ANS + "\n");
-		bw.flush(); bw.close();
-	}
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) >= 48)
+            n = (n << 3) + (n << 1) + (c & 15);
+        if (c == 13) System.in.read();
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) A[i] = read();
+
+        ArrayList<Integer> dp = new ArrayList<>();
+
+        for (int num : A) {
+            int idx = Collections.binarySearch(dp, num);
+            if (idx < 0) idx = -idx - 1;
+
+            if (idx == dp.size()) {
+                dp.add(num);
+            } else {
+                dp.set(idx, num);
+            }
+        }
+
+        bw.write(dp.size() + "\n");
+        bw.flush();
+        bw.close();
+    }
 }
