b72630af803aba49143c95127088f2b9de7785de
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-08 17:12:50 +0900

    [Gold IV] Title: 스도쿠, Time: 328 ms, Memory: 16152 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -5,69 +5,70 @@ public class Main {
 
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	private static int read() throws Exception {
-	    int c; while ((c = System.in.read()) != -1) if ('0' <= c && c <= '9') return c - '0';
-	    return -1;
+		int c;
+		while ((c = System.in.read()) != -1) if ('0' <= c && c <= '9') return c - '0';
+		return -1;
 	}
-	
-	static boolean isComplete;
+
 	static int[][] map = new int[9][9];
-	static boolean[][][] iv = new boolean[3][9][10];
-	static List<int[]> inComplete = new ArrayList<>();
-	
+	static int[] rowBit = new int[9], colBit = new int[9], sqrBit = new int[9];
+	static List<int[]> inComp = new ArrayList<>();
+	static boolean done;
+
 	private static void init() throws Exception {
 		for (int i = 0; i < 9; i++) for (int j = 0; j < 9; j++) {
-			int in = read();
-			map[i][j] = in;
-			if (in == 0) inComplete.add(new int[] { i, j });
+			int n = read();
+			map[i][j] = n;
+			if (n == 0) inComp.add(new int[] { i, j });
 			else {
-				int squareIdx = (i / 3) * 3 + (j / 3);
-				iv[0][i][in] = true;
-				iv[1][j][in] = true;
-				iv[2][squareIdx][in] = true;
+				int sqr = (i / 3) * 3 + (j / 3);
+				rowBit[i] |= 1 << n;
+				colBit[j] |= 1 << n;
+				sqrBit[sqr] |= 1 << n;
 			}
 		}
 	}
 
 	private static void printer() throws Exception {
 		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) bw.write(Integer.toString(map[i][j])); 
+			for (int j = 0; j < 9; j++) bw.write(Integer.toString(map[i][j]));
 			bw.write("\n");
 		}
 	}
-	
-	private static void backtrack(int depth) throws Exception {
-		if (depth == inComplete.size()) {
+
+	private static void bt(int d) throws Exception {
+		if (done) return;
+		if (d == inComp.size()) {
 			printer();
-			isComplete = true;
+			done = true;
 			return;
 		}
 		
-		if (isComplete) return;
-		
-		int[] cur = inComplete.get(depth);
-		int x = cur[0], y = cur[1], squareIdx = (x/3)*3 + (y/3);
-		
+		int[] cur = inComp.get(d);
+		int x = cur[0], y = cur[1], s = (x / 3) * 3 + (y / 3);
+
 		for (int n = 1; n <= 9; n++) {
-			if (iv[0][x][n] || iv[1][y][n] || iv[2][squareIdx][n]) continue; // check if number n is already used,
-			
-			iv[0][x][n] = iv[1][y][n] = iv[2][squareIdx][n] = true; // if not used, use this number,
-			
-			map[x][y] = n; // mark on the puzzle,
-			
-			backtrack(depth+1); // backtrack,
-			
-			if (isComplete) return; // pruning,
-			
-			iv[0][x][n] = iv[1][y][n] = iv[2][squareIdx][n] = false; // cancel this number for backtrack,
-			
-			map[x][y] = 0; // cancel this number from the puzzle,
+			int bit = 1 << n;
+			if ((rowBit[x] & bit) != 0 || (colBit[y] & bit) != 0 || (sqrBit[s] & bit) != 0) continue;
+
+			map[x][y] = n;
+			rowBit[x] ^= bit;
+			colBit[y] ^= bit;
+			sqrBit[s] ^= bit;
+
+			bt(d + 1);
+			if (done) return;
+
+			map[x][y] = 0;
+			rowBit[x] ^= bit;
+			colBit[y] ^= bit;
+			sqrBit[s] ^= bit;
 		}
-		
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		init();
-		backtrack(0);
+		bt(0);
 		bw.flush(); bw.close();
 	}
 }
