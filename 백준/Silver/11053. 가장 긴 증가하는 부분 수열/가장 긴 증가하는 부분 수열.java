04386e37726d561210d39d7e74f22e7c3a4dfdd3
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-08 10:15:21 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,38 +1,34 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    
-    private static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) >= 48)
-            n = (n << 3) + (n << 1) + (c & 15);
-        if (c == 13) System.in.read();
-        return n;
-    }
-
-    public static void main(String[] args) throws Exception {
-        int N = read();
-        int[] A = new int[N];
-        for (int i = 0; i < N; i++) A[i] = read();
-
-        ArrayList<Integer> dp = new ArrayList<>();
-
-        for (int num : A) {
-            int idx = Collections.binarySearch(dp, num);
-            if (idx < 0) idx = -idx - 1;
-
-            if (idx == dp.size()) {
-                dp.add(num);
-            } else {
-                dp.set(idx, num);
-            }
-        }
-
-        bw.write(dp.size() + "\n");
-        bw.flush();
-        bw.close();
-    }
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static int read() throws Exception {
+		int c, n = System.in.read() & 15;
+		while ((c=System.in.read())>=48) n = (n<<3) + (n<<1) + (c&15);
+		if (c==13) System.in.read();
+		return n;
+	}
+	
+	public static void main(String[] args) throws Exception {
+	
+		int len = 0, N = read(), dp[] = new int[N], A[] = new int[N];
+		
+		for (int i = 0; i < N; i++) A[i] = read();
+		
+		for (int i = 0; i < N; i++) {
+			int num = A[i], lo = 0, hi = len;
+			
+			while (lo < hi) {
+				int mid = (lo + hi) >>> 1;
+				if (dp[mid] < num) lo = mid + 1; 
+                else hi = mid;
+			}
+			
+			dp[lo] = num;
+			if (lo == len) len++;
+		}
+		
+		bw.write(len + "\n"); bw.flush(); bw.close();
+	}
 }
