1102a57cbf1b1b14789f5067a051780633dd8a90
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-09 10:43:34 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,342 ms, Memory: 65,904 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Solution {
 	
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int[][] adj;
+	static int N;
 
 	private static int read() throws Exception {
 		int c = System.in.read(), n = 0, sign = 1;
@@ -14,35 +16,52 @@ public class Solution {
 	}
 
 	public static void main(String[] args) throws Exception {
+		int T = read();
 		
-		int INF = 1_000_000_000, T = read();
 		for (int tc = 1; tc <= T; tc++) {
+			N = read();
+			adj = new int[N][N];
 			
-			int N = read(), dist[][] = new int[N][N], cc[] = new int[N];
-			
-			for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) {
-				dist[i][j] = (i==j) ? 0 : INF;
+			// 인접행렬 입력
+			for (int i = 0; i < N; i++)
+				for (int j = 0; j < N; j++)
+					adj[i][j] = read();
+
+			int min = Integer.MAX_VALUE;
+
+			// 각 노드에서 BFS 수행
+			for (int i = 0; i < N; i++) {
+				int sum = bfs(i);
+				min = Math.min(min, sum);
 			}
-			
-			for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) if (read() == 1) dist[i][j] = 1;
-			
-			for (int k = 0; k < N; k++) {
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < N; j++) {
-						if (dist[i][j] > dist[i][k] + dist[k][j]) dist[i][j] = dist[i][k] + dist[k][j];
-					}
+
+			bw.write("#" + tc + " " + min + "\n");
+		}
+		bw.flush(); bw.close();
+	}
+
+	// BFS 수행: 시작 노드로부터 각 노드까지 거리 합 리턴
+	private static int bfs(int start) {
+		boolean[] vis = new boolean[N];
+		int[] dist = new int[N];
+		Queue<Integer> q = new ArrayDeque<>();
+		
+		q.offer(start);
+		vis[start] = true;
+		
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			for (int next = 0; next < N; next++) {
+				if (!vis[next] && adj[cur][next] == 1) {
+					vis[next] = true;
+					dist[next] = dist[cur] + 1;
+					q.offer(next);
 				}
 			}
-			
-			for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) {
-				cc[i] += dist[i][j];
-			}
-			
-			int min = INF; for (int i = 0; i < N; i++) min = Math.min(min, cc[i]);
-
-			bw.write("#"+tc+" "+min+"\n");
 		}
-		
-		bw.flush();bw.close();
+
+		int sum = 0;
+		for (int d : dist) sum += d;
+		return sum;
 	}
 }
