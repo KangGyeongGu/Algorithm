716bb97ec80c80751b861a34f1de7d43a0fa8864
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-04-09 14:11:25 +0900

    [Gold III] Title: 역사, Time: 404 ms, Memory: 16380 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.java
+++ 백준/Gold/1613. 역사/역사.java
@@ -1,7 +1,3 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class Main {
 
 	static StringBuilder sb = new StringBuilder();
@@ -15,32 +11,27 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		int N = read(), K = read();
-		int[][] dist = new int[N+1][N+1];
+		int[][] dist = new int[N+1][N+1]; // 1-based,
 		
-		for (int i = 0; i < K; i++) {
+		for (int i = 0; i < K; i++) { // read input,
 			int u = read(), v = read();
-
-			dist[u][v] = -1;
-			dist[v][u] = 1;
+			dist[u][v] = -1; // if u -> v, then -1
+			dist[v][u] = 1; // and also u <- v is 1
 		}
 		
-		
-		
+		// Floyd
 		for (int k = 1; k <= N; k++) {
 			for (int i = 1; i <= N; i++) {
 				if (i==k) continue;
 				for (int j = 1; j <= N; j++) {
 					if (j==k || j==i) continue;
-					if (dist[i][k] != 0 && dist[i][k] == dist[k][j]) dist[i][j] = dist[i][k];
+					if (dist[i][k] != 0 && dist[i][k] == dist[k][j]) dist[i][j] = dist[i][k]; // if i->k and k->j then i->j
 				}
 			}
 		}
 		
 		int S = read();
-		for (int i = 0; i < S; i++) {
-			int u = read(), v = read();
-			sb.append(dist[u][v]).append('\n');
-		}
+		for (int i = 0; i < S; i++) sb.append(dist[read()][read()]).append('\n');
 		System.out.println(sb);
 	}
 }
