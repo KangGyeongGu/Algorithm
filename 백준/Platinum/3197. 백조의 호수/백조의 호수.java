fb976792f995cefe34975a0eb6d79f14d0185f13
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-06-02 12:31:39 +0900

    [Platinum V] Title: 백조의 호수, Time: 856 ms, Memory: 133468 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -1,136 +1,129 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int R, C;
-    static char[][] map;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    
-    // 두 개의 큐가 핵심!
-    static Queue<Point> waterQueue = new LinkedList<>();  // 빙판 녹이기용
-    static Queue<Point> swanQueue = new LinkedList<>();   // 백조 이동용
-    
-    static class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+
+    static class Coord {
+        int r, c;
+        Coord (int r, int c) {
+            this.r = r;
+            this.c = c;
         }
     }
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        R = sc.nextInt();
-        C = sc.nextInt();
+
+    static int R, C;
+    static char[][] map;
+    static boolean[][] isVisited;
+    static final int[][] DIR = { {1,0}, {-1,0}, {0,1}, {0,-1} };
+
+    static List<Coord> swans = new ArrayList<>();
+
+    static Queue<Coord> waterQueue = new ArrayDeque<>();
+    static Queue<Coord> swanQueue = new ArrayDeque<>();
+
+    private static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
         map = new char[R][C];
-        visited = new boolean[R][C];
-        
-        List<Point> swans = new ArrayList<>();
-        
-        // 초기 설정
-        for (int i = 0; i < R; i++) {
-            String line = sc.next();
-            for (int j = 0; j < C; j++) {
-                map[i][j] = line.charAt(j);
-                
-                if (map[i][j] == 'L') {
-                    swans.add(new Point(i, j));
-                    map[i][j] = '.'; // 백조 위치도 물로 취급
+        isVisited = new boolean[R][C];
+
+        for (int r = 0; r < R; r++) {
+            String in = br.readLine();
+
+            for (int c = 0; c < C; c++) {
+                char ch = in.charAt(c);
+
+                if (ch == '.') {
+                    waterQueue.offer(new Coord(r, c));
+                    map[r][c] = ch;
                 }
-                
-                if (map[i][j] == '.') {
-                    waterQueue.add(new Point(i, j)); // 초기 물 위치들
+
+                else if (ch == 'L') {
+                    swans.add(new Coord(r, c));
+                    waterQueue.add(new Coord(r, c));
+                    map[r][c] = '.';
+                }
+
+                else map[r][c] = ch;
+            }
+        }
+    }
+
+    private static boolean isSwanCanMeet(Coord end) {
+        Queue<Coord> Q = new ArrayDeque<>();
+
+        while (!swanQueue.isEmpty()) {
+            Coord curr = swanQueue.poll();
+
+            if (curr.r == end.r && curr.c == end.c) {
+                return true;
+            }
+
+            for (int[] dir : DIR) {
+                int nr = curr.r + dir[0];
+                int nc = curr.c + dir[1];
+
+                if (nr < 0 || nr >= R || nc < 0 || nc >= C || isVisited[nr][nc]) continue;
+
+                isVisited[nr][nc] = true;
+
+                if (map[nr][nc] == '.') swanQueue.offer(new Coord(nr, nc));
+
+                else Q.offer(new Coord(nr, nc));
+            }
+        }
+
+        swanQueue = Q;
+        return false;
+    }
+
+    private static void meltDownIce() {
+        int qSize = waterQueue.size();
+
+        for (int i = 0; i < qSize; i++) {
+            Coord curr = waterQueue.poll();
+
+            for (int[] dir : DIR) {
+                int nr = curr.r + dir[0];
+                int nc = curr.c + dir[1];
+
+                if (nr < 0 || nr >= R || nc < 0 || nc >= C) continue;
+
+                if (map[nr][nc] == 'X') {
+                    map[nr][nc] = '.';
+                    waterQueue.offer(new Coord(nr, nc));
                 }
             }
         }
-        
-        // 첫 번째 백조에서 시작하는 BFS 초기화
-        Point start = swans.get(0);
-        Point end = swans.get(1);
-        
-        swanQueue.add(start);
-        visited[start.x][start.y] = true;
-        
+    }
+
+    private static void run() {
+        Coord start = swans.get(0);
+        Coord end = swans.get(1);
+
+        swanQueue.offer(start);
+        isVisited[start.r][start.c] = true;
+
         int days = 0;
-        
+
         while (true) {
-            // 1단계: 백조가 현재 갈 수 있는 모든 곳을 탐색
-            if (canMeet(end)) {
+
+            if (isSwanCanMeet(end)) {
                 System.out.println(days);
-                break;
+                return;
             }
-            
-            // 2단계: 하루가 지나고 빙판을 녹임
-            meltIce();
+
+            meltDownIce();
             days++;
         }
     }
-    
-    /**
-     * 백조 이동 BFS
-     * 현재 갈 수 있는 모든 물 공간을 탐색하고,
-     * 빙판을 만나면 다음날을 위해 큐에 저장
-     */
-    static boolean canMeet(Point target) {
-        Queue<Point> nextQueue = new LinkedList<>();
-        
-        while (!swanQueue.isEmpty()) {
-            Point curr = swanQueue.poll();
-            
-            // 목표 지점에 도달했는지 확인
-            if (curr.x == target.x && curr.y == target.y) {
-                return true;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = curr.x + dx[i];
-                int ny = curr.y + dy[i];
-                
-                if (nx < 0 || nx >= R || ny < 0 || ny >= C || visited[nx][ny]) {
-                    continue;
-                }
-                
-                visited[nx][ny] = true;
-                
-                if (map[nx][ny] == '.') {
-                    // 물이면 즉시 이동 가능
-                    swanQueue.add(new Point(nx, ny));
-                } else {
-                    // 빙판이면 다음날 녹을 예정이므로 다음 큐에 저장
-                    nextQueue.add(new Point(nx, ny));
-                }
-            }
-        }
-        
-        // 다음날을 위해 큐 교체
-        swanQueue = nextQueue;
-        return false;
-    }
-    
-    /**
-     * 빙판 녹이는 BFS
-     * 현재 물과 접촉한 모든 빙판을 물로 변경
-     */
-    static void meltIce() {
-        int size = waterQueue.size();
-        
-        for (int i = 0; i < size; i++) {
-            Point curr = waterQueue.poll();
-            
-            for (int j = 0; j < 4; j++) {
-                int nx = curr.x + dx[j];
-                int ny = curr.y + dy[j];
-                
-                if (nx < 0 || nx >= R || ny < 0 || ny >= C) {
-                    continue;
-                }
-                
-                if (map[nx][ny] == 'X') {
-                    map[nx][ny] = '.'; // 빙판을 물로 변경
-                    waterQueue.add(new Point(nx, ny)); // 새로운 물 위치 추가
-                }
-            }
-        }
+
+    public static void main(String[] args) throws Exception {
+        init();
+        run();
     }
 }
