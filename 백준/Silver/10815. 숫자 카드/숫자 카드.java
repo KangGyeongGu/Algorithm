81a6a425b21f6c3b267ec192b11ee9611cc3e4cd
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2025-06-10 11:32:03 +0900

    [Silver V] Title: 숫자 카드, Time: 824 ms, Memory: 135148 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -4,63 +4,26 @@ import java.util.*;
 public class Main {
 
     static int N, M;
-    static int[] S, G;
+    static Set<Integer> S = new HashSet<>();
 
-    static StringBuilder sb = new StringBuilder();
-
-    private static void init() throws Exception {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        S = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int n = 0; n < N; n++) {
-            S[n] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            S.add(Integer.parseInt(st.nextToken()));
         }
 
         M = Integer.parseInt(br.readLine());
-        G = new int[M];
         st = new StringTokenizer(br.readLine());
-        for (int m = 0; m < M; m++) {
-            G[m] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(S);
-    }
-
-    private static boolean binarySearch(int target) {
-        int left = 0, right = S.length-1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (S[mid] == target) {
-                return true;
-            } else if (S[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return false;
-    }
-
-    private static void run() {
-        for (int m = 0; m < M; m++) {
-            if (binarySearch(G[m])) {
-                sb.append("1").append(" ");
-            } else {
-                sb.append("0").append(" ");
-            }
+        for (int i = 0; i < M; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(S.contains(target) ? "1 " : "0 ");
         }
 
         System.out.println(sb);
-    }
-
-    public static void main(String[] args) throws Exception {
-        init();
-        run();
     }
 }
