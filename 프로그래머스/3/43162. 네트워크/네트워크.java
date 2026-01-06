463546a38b7df071e8903c48d388f54fb359248e
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2026-01-06 11:11:19 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(i, n, computers, visited);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private void bfs(int start, int n, int[][] computers, boolean[] visited) {
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(start);
+        visited[start] = true;
+        
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            
+            for (int i = 0; i < n; i++) {
+                if (computers[curr][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    q.offer(i);
+                }
+            }
+            
+        }
+        
+    }
+}
