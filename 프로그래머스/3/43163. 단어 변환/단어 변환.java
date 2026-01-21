53d5899900bc66c1c517f5b1f026c0d4a24ebd82
KangGyeongGu <140485408+KangGyeongGu@users.noreply.github.com>
2026-01-21 15:24:14 +0900

    [level 3] Title: 단어 변환, Time: 0.59 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,59 @@
 import java.util.*;
 
 class Solution {
+    
+    static class Node {
+        String word;
+        int depth;
+        
+        Node(String word, int depth) {
+            this.word = word;
+            this.depth = depth;
+        }
+    }
+    
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        if (!isValidCase(target, words)) return 0;
+        return bfs(begin, target, words);
+    }
+    
+    private boolean isValidCase(String target, String[] words) {
+        for (String word : words) {
+            if (word.equals(target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean canChange(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) diff++;
+        }
+        return diff == 1;
+    }
+    
+    private int bfs(String begin, String target, String[] words) {
         
         boolean[] visited = new boolean[words.length];
-        Deque<String> dq = new ArrayDeque<>();
-        Deque<Integer> depth = new ArrayDeque<>();
+        Queue<Node> dq = new ArrayDeque<>();
         
-        dq.offer(begin);
-        depth.offer(0);
+        dq.offer(new Node(begin, 0));
         
-        while (!dq.isEmpty()) {
-            String curr = dq.poll();
-            int d = depth.poll();
+        while(!dq.isEmpty()) {
+            Node curr = dq.poll();
             
-            if (curr.equals(target)) {
-                return d;
-            }
+            if (curr.word.equals(target)) return curr.depth;
             
             for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && isOneDiff(curr, words[i])) {
-                    dq.offer(words[i]);
+                if (!visited[i] && canChange(curr.word, words[i])) {
                     visited[i] = true;
-                    depth.offer(d+1);
+                    dq.offer(new Node(words[i], curr.depth+1));
                 }
             }
         }
         
         return 0;
-    }
-    
-    private boolean isOneDiff(String a, String b) {
-        int diff = 0;
-        
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) diff++;
-            if (diff > 1) return false;
-        }
-        
-        return diff == 1;
     }
 }
